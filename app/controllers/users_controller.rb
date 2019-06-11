3fa75a6aad885eb6017df1342eb701c49a423cf6
paramagicdev <konnor5456@gmail.com>
2019-06-11 04:09:36 -0400

    Adding styling similar to FB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
   def home
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :static_pages
+  resources :users
 end
