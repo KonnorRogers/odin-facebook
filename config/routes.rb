d42f2169629a13a9bd13f7fe859ba14929288495
paramagicdev <konnor5456@gmail.com>
2019-06-14 19:01:13 -0400

    renaming routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     post '/signout', to: 'devise/regisrations#create'
 
     get '/login', to: 'devise/sessions#new'
+    post '/login', to: 'devise/sessions#create'
     delete '/logout', to: 'devise/sessions#destroy'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
