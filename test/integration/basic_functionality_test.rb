3753ccaa085bb46ba5368c11fe7dc1abfc2d09f4
paramagicdev <konnor5456@gmail.com>
2019-06-16 11:14:31 -0400

    Tests work now, fixed users fixtures

--- test/integration/basic_functionality_test.rb
+++ test/integration/basic_functionality_test.rb
@@ -3,5 +3,12 @@ require 'test_helper'
 class BasicFunctionalityTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
+  def setup
+    @user = users(:bob)
+  end
 
+  test 'typical user signup' do
+    get root_url
+    assert_redirected_to signup_path
+  end
 end
