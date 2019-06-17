641c77a562831149352c5aba30a8375d79d7698f
paramagicdev <konnor5456@gmail.com>
2019-06-17 14:45:32 -0400

    fixing migrations

--- db/migrate/20190611065547_add_devise_to_users.rb
+++ db/migrate/20190611065547_add_devise_to_users.rb
@@ -4,7 +4,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email
+      t.change :email, :string, :null => false, :default => ""
       t.string :encrypted_password
 
 
@@ -38,6 +38,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
       # t.timestamps null: false
     end
 
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
