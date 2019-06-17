840a9f802b0a518d8bcf41c6db6ca6377a1c7799
paramagicdev <konnor5456@gmail.com>
2019-06-17 16:56:55 -0400

    Added friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
   GENDERS = %i[male female other].freeze
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
