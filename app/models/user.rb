633ce3cb23c435f3d43eb24729ed2dd780671b71
paramagicdev <konnor5456@gmail.com>
2019-06-17 17:05:18 -0400

    Added a friend request model, good spot to stop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :friend_requests, dependent: :destroy
+  has_many :pending_friends, through: :friend_requests,
+                             source: :friend
+
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
 
