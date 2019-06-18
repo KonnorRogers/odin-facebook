e3111e332ccfb688ba1e3757d61e9784077477d8
paramagicdev <konnor5456@gmail.com>
2019-06-18 09:57:06 -0400

    Adding a friend integration test

--- db/migrate/20190618122633_create_friend_requests.rb
+++ db/migrate/20190618122633_create_friend_requests.rb
@@ -2,9 +2,10 @@ class CreateFriendRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :friend_requests do |t|
       t.references :user, foreign_key: true
-      t.references :friend, foreign_key: true
+      t.references :friend, foreign_key: true, class: "User"
 
       t.timestamps
     end
+
   end
 end
