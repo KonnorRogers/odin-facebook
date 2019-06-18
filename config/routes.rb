b38e8b05913c9051acce9a6a4c57ca299c323033
paramagicdev <konnor5456@gmail.com>
2019-06-18 16:28:48 -0400

    Create friendships controller, add to routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
-  resources :friends
+  resources :friendships
 end
