8d0fab6be7e702094da3e15a8dea0a46ca59266c
paramagicdev <konnor5456@gmail.com>
2019-06-22 17:14:42 -0400

    Adding things to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     if user_signed_in?
+      @users = User.all
       @received_requests = current_user.received_requests
       @sent_requests = current_user.sent_requests
       @friends = current_user.all_friends
