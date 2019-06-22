c4fe7ae0ee810e6a933293d96422552ab3b49121
paramagicdev <konnor5456@gmail.com>
2019-06-22 15:19:17 -0400

    issues with users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def index
-    redirect_to signup_path unless user_signed_in?
-    @sent_requests = current_user.sent_requests
-    @received_requests = current_user.received_requests
-    @friends = current_user.all_friends
+    if user_signed_in?
+      redirect_to signup_path
+    else
+      # @sent_requests = current_user.sent_requests
+      # @received_requests = current_user.received_requests
+      # @friends = current_user.all_friends
+    end
   end
 end
