1409f6c5a5cadd5652f37f1ff5bd6e46df1d2cb8
paramagicdev <konnor5456@gmail.com>
2019-06-23 21:03:18 -0400

    Adding user functionality

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -3,7 +3,7 @@ class FriendRequestsController < ApplicationController
     @friendship = current_user.friend_requests.build(friend_id: params[:friend_id])
     @friend = User.find(params[:friend_id])
     if @friendship.save
-      flash[:notice] = "You have accepted #{@friend}"
+      flash[:notice] = "You have sent a friend request to #{@friend.full_name}"
     else
       flash[:error] = 'Unable to add friend'
     end
@@ -12,7 +12,7 @@ class FriendRequestsController < ApplicationController
   end
 
   def destroy
-    @friendship = current_user.friend_request.find(params[:id])
+    @friendship = current_user.friend_requests.find(params[:id])
     @friend = User.find(params[:id])
     @friendship.destroy
     flash[:notice] = "You are no longer friends with #{@friend}"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
   def index
     if user_signed_in?
-      @users = User.all
+      @users = User.where.not(id: current_user.id)
       @received_requests = current_user.received_requests
       @sent_requests = current_user.sent_requests
       @friends = current_user.all_friends
     else
       redirect_to signup_path
     end
+  end
+
+  def show
+    @user = current_user
   end
 end
