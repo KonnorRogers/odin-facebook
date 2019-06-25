8009e437b1d2d9438120dfb60a96fac8883cbd98
paramagicdev <konnor5456@gmail.com>
2019-06-25 17:47:34 -0400

    Fix friendships to friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
       end
       @received_requests = current_user.received_requests
       @sent_requests = current_user.sent_requests
-      @friends = current_user.all_friendships
+      @friends = current_user.all_friends
     else
       redirect_to signup_path
     end
