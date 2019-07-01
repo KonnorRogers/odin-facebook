d1bf2afe9cabdf2139007b4f8a3b5d89405805ef
paramagicdev <konnor5456@gmail.com>
2019-07-01 01:35:44 -0400

    Add dropdown support

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,13 +1,13 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function dropDown() {
-  document.getElementById("dropdown-notifications").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-    const dropdowns = document.getElementsByClassName("dropdown-links");
+    const dropdowns = document.getElementsByClassName("dropdown-content");
     let i;
     for (i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
