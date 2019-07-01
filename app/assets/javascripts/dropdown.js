abd458cd7d5b3b6c2030d1e79ffa4c1401165e72
paramagicdev <konnor5456@gmail.com>
2019-07-01 18:17:25 -0400

    cleaning up some javascript for dropdown

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,19 +1,19 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+/* When the user clicks on the button, toggle between hiding and showing the dropdown content */ 
 function dropDown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    const dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+window.onclick = function(e) {
+  if (e.target.matches('.dropbtn')) { return false };
+
+  const myDropdown = document.getElementById("myDropdown");
+  if (myDropdown.classList.contains('show')) {
+    myDropdown.classList.remove('show');
   }
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  let dropbtn = document.getElementById("drop-btn");
+  dropbtn.addEventListener("click", dropDown, false);
+});
