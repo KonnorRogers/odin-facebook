45183bb8576012aa8628f49b7210c8be9a462c74
paramagicdev <konnor5456@gmail.com>
2019-07-01 19:27:12 -0400

    Add associations to noficiations

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Notification < ApplicationRecord
+  belongs_to :recipient, class_name: 'User'
+  belongs_to :sender, class_name: 'User'
+  belongs_to :notifiable, polymorphic: true
 end
