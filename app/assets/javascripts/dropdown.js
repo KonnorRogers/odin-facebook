d0784727c8848fb7804e05d978da90e6fdcae508
paramagicdev <konnor5456@gmail.com>
2019-06-30 21:04:49 -0400

    adding a dropdown

---
+++ app/assets/javascripts/dropdown.js
@@ -0,0 +1,24 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+// function dropdown() {
+//   document.getElementById("my-dropdown").classList.toggle("show");
+// }
+
+// Close the dropdown menu if the user clicks outside of it
+// window.onclick = (event) => {
+//   if (event.target.matches('.dropbtn')); return
+
+//   const dropdowns = document.getElementsByClassName("dropdown-content");
+//   for (let i = 0; i < dropdowns.length; i++) {
+//     let openDropdown = dropdowns[i];
+//     if (openDropdown.classList.contains('show')) {
+//       openDropdown.classList.remove('show');
+//     }
+//   }
+// };
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   console.log("DOMContentLoaded");
+// });
+
+console.log("Hello from the DOM");
