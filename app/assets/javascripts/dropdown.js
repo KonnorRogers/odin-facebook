bd610a28842412a6fcd2ba688cad99cb4cf5e7f3
paramagicdev <konnor5456@gmail.com>
2019-06-30 22:38:52 -0400

    implementing notifications

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,24 +1,19 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-// function dropdown() {
-//   document.getElementById("my-dropdown").classList.toggle("show");
-// }
+function dropDown() {
+  document.getElementById("dropdown-notifications").classList.toggle("show");
+}
 
-// Close the dropdown menu if the user clicks outside of it
-// window.onclick = (event) => {
-//   if (event.target.matches('.dropbtn')); return
-
-//   const dropdowns = document.getElementsByClassName("dropdown-content");
-//   for (let i = 0; i < dropdowns.length; i++) {
-//     let openDropdown = dropdowns[i];
-//     if (openDropdown.classList.contains('show')) {
-//       openDropdown.classList.remove('show');
-//     }
-//   }
-// };
-
-// document.addEventListener('DOMContentLoaded', (event) => {
-//   console.log("DOMContentLoaded");
-// });
-
-console.log("Hello from the DOM");
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    const dropdowns = document.getElementsByClassName("dropdown-links");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
