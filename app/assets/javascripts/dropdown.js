9b9e6ebbbbe4a3a37b55ee5a85268ef7be3ec814
paramagicdev <konnor5456@gmail.com>
2019-07-05 19:20:36 -0400

    fixed a javascript issue

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -5,6 +5,7 @@ function dropDown() {
 
 window.onclick = function(e) {
   if (e.target.matches('.dropbtn')) { return false };
+  if (e.target.closest('.dropbtn')) { return false };
 
   const myDropdown = document.getElementById("myDropdown");
   if (myDropdown.classList.contains('show')) {
