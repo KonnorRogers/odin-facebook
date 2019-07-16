511d6b6734acdb2c576d954583507b4ea337a47e
paramagicdev <konnor5456@gmail.com>
2019-07-16 07:00:13 -0400

    solid progress on post feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,18 @@ class UsersController < ApplicationController
   def index
     if user_signed_in?
       @post = current_user.posts.build
+      @feed = current_user.feed
+
+      ####################################################
+      # All this will be stripped out later
+      ####################################################
       @users = User.where.not(id: current_user.id).reject do |user|
         user.friend?(current_user) || user.pending_request?(current_user)
       end
       @received_requests = current_user.received_requests
       @sent_requests = current_user.sent_requests
       @friends = current_user.all_friends
+      ####################################################
     else
       redirect_to signup_path
     end
