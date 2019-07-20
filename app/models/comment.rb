02b453ccb5605ac9352998650d9da3f81157863d
paramagicdev <konnor5456@gmail.com>
2019-07-20 17:43:12 -0400

    added comment testing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :content, presence: true,
+                      length: { maximum: 140 }
+
+  default_scope -> { order(created_at: :desc) }
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:bob)
+    @post = posts(:one)
+  end
+
+  test 'should add a comment to the post' do
+    content = 'this is my first comment'
+
+    assert_difference 'Comment.count', 1 do
+      @post.comments.create(user: @user, content: content)
+    end
+  end
+
+  test 'should not add a comment to the post > 140 characters' do
+    assert_no_difference 'Comment.count' do
+      content = 'h' * 141
+      @post.comments.create(user: @user, content: content)
+    end
+  end
+
+  test 'should not add a blank comment to the post' do
+    assert_no_difference 'Comment.count' do
+      content = 'h' * 141
+      @post.comments.create(user: @user, content: content)
+    end
+  end
 end
