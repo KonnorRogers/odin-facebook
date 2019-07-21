6cd3ab3ca0c8dc213cb32e280615599e81fbb2b6
paramagicdev <konnor5456@gmail.com>
2019-07-21 19:56:48 -0400

    added likeable polymorphic association to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
 
-  belongs_to :likable, polymorphic: true
+  belongs_to :likeable, polymorphic: true
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
