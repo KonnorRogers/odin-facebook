b06137acd98338d7e22cfb657211b0bd748c291c
paramagicdev <konnor5456@gmail.com>
2019-07-21 19:37:02 -0400

    added comment to validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,6 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  # Validates that the user has only liked the post 1 time
   validates :user, uniqueness: { scope: :post }
 end
