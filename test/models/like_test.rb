e87a82a714ce94d191aff89b9453c7a04cf1ade4
paramagicdev <konnor5456@gmail.com>
2019-07-23 00:05:11 -0400

    getting like tests to work

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -11,4 +11,21 @@ class LikeTest < ActiveSupport::TestCase
       @post.likes.create(user: @user)
     end
   end
+
+  test 'should not add a second like to the post' do
+    @post.likes.create(user: @user)
+    assert_no_difference 'Like.count' do
+      @post.likes.create(user: @users)
+    end
+  end
+
+  test 'adds the like if the user unlikes the object' do
+    assert_difference 'Like.count', 1 do
+      @post.likes.create(user: @user)
+    end
+
+    assert_difference 'Like.count', 1 do
+      @post.likes.destroy(user: @user)
+    end
+  end
 end
