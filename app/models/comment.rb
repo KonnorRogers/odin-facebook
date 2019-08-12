d6e550112db993bbaad28da8161ff9199f37a3ff
paramagicdev <konnor5456@gmail.com>
2019-08-12 02:28:46 -0400

    Wrapping head around comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
+# https://dev.to/lucysuddenly/nested-comments-in-ruby-on-rails-1k1f
 class Comment < ApplicationRecord
   belongs_to :user
+  # Is the parent of the comment
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
 
