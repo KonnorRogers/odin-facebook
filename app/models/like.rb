56142fbb3e1d07871e96d00823dc17bd466e9307
paramagicdev <konnor5456@gmail.com>
2019-08-28 19:14:52 -0400

    Add link to gorails in like

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,5 +3,6 @@ class Like < ApplicationRecord
   belongs_to :likeable, polymorphic: true
 
   # Validates that the user has only liked the post 1 time
+  # https://github.com/gorails-screencasts/gorails-24-liking-posts/blob/master/
   validates :user, uniqueness: { scope: :likeable }
 end
