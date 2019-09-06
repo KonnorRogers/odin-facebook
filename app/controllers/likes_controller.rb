10ae3ea10fb0f5838abc8ab659e4403336d72fc5
paramagicdev <konnor5456@gmail.com>
2019-09-06 02:42:07 -0400

    Adding notifications

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 class LikesController < ApplicationController
   before_action :set_post
 
   def create
     @post.likes.find_or_create_by(user_id: current_user.id)
+
+    # Send notification
+    Notification.create(recipient: @post.author, sender: current_user,
+                        action: 'liked', notifiable: 'Post')
 
     respond_to do |fmt|
       fmt.html { redirect_back(fallback_location: root_url) }
