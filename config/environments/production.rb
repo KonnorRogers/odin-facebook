b107bad8917fa39fde32bb6073f3fd02c86af599
paramagicdev <konnor5456@gmail.com>
2019-09-07 06:02:47 -0400

    configure sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,19 +66,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "odin-facebook_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {
-    host: 'https://paramagic-facebook.herokuapp.com'
-  }
+
   # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
+
+  host = 'paramagic-app.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'paramagic-app.herokuapp.com',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: :plain,
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
 
